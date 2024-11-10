81b5182991372325dcda6bf4431c28d9f4ad74db
ytapasco88 <ytapasco88@gmail.com>
2024-11-09 20:44:22 -0500

    Hola Mundo Go

---
+++ godesde0/go.mod
@@ -0,0 +1,3 @@
+module Ecommerce_Go/godesde0
+
+go 1.23.3
---
+++ godesde0/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* Hola Mundo */
+func main() {
+	fmt.Println("Hola, Mundo") /* Impresion en Consola */
+
+}
